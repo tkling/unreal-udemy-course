42503dc15668289a1c948ed516e32e3ae8270ba6
Tyler Kling <tyling@gmail.com>
2018-06-10 11:49:26 -0400

    add door opening from code (OpenDoor), adjust starting view to look at door

---
+++ Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,34 @@
+// No copyright info at the moment.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor *owner = GetOwner();
+	FRotator rotationValues = FRotator(0.f, 60.f, 0.f);
+	owner->AddActorWorldRotation(rotationValues);	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
